791a004a69957df1e108511aca933d96a7521265
Freed-Wu <Freed-Wu@users.noreply.github.com>
2025-01-01 01:05:17 +0000

    Auto update references for commits, tags, etc

--- scripts/fetch-docs.sh
+++ scripts/fetch-docs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Tag: yocto-4.3.3
-BITBAKE_DOCS_COMMIT=380a9ac97de5774378ded5e37d40b79b96761a0c
-# Tag: yocto-4.3.3
-YOCTO_DOCS_COMMIT=dde4b815db82196af086847f68ee27d7902b4ffa
+# Tag: yocto-5.1.1
+BITBAKE_DOCS_COMMIT=9602a684568910fd333ffce907fa020ad3661c26
+    The previous commit fixed persist_data's context manager to close the
+    (cherry picked from commit 61f803c7d92a012b62837b0cdae4789a394b260e)
+# Tag: yocto-5.1.1
+YOCTO_DOCS_COMMIT=8288c8cae7fe7303e89d8ed286de91fc26ce6cc3
+    (cherry picked from commit ed49067f172637d38d470a864feed2a02bd84c66)
 
 BITBAKE_DOCS_LIST="bitbake-user-manual-metadata.rst bitbake-user-manual-ref-variables.rst"
 YOCTO_DOCS_LIST=" tasks.rst variables.rst"
